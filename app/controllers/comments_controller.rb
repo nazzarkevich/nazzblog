f24266fb66d76b4c8452c85e75df0ac8a0ce970d
Nazar Tkach <nazzik.3@gmail.com>
2016-04-01 14:20:57 +0300

    Edit comment delete issue and About page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
 	def destroy
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.find(params[:id])
-		@comments.destroy
+		@comment.destroy
 
 		redirect_to post_path(@post)
 	end
